011a6f0eca21d178999e28a1cbfc57d2a15e4a72
olga2710 <olga.liaz@yandex.ru>
2021-10-10 19:25:24 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 const dateItems = document.querySelectorAll('.session__date-item');
+const schemeSvg = document.querySelector('.scheme-svg');
+const spanCounter = document.querySelector('.span-counter');
+let cost = 800;
+let totalCost=0;
+
 
 dateItems.forEach(item =>{
     item.addEventListener('click', ()=>{
         dateItems.forEach(item=>{
             item.classList.remove('session__date-item_active');
         });
-        item.classList.add('session__date-item_active');
+        item.classList.add('session__date-item_active'); 
 
     })
-})
+});
+
+
+schemeSvg.addEventListener('click', (ev)=>{
+
+    if (!ev.target.classList.contains('booked')){
+        ev.target.classList.toggle('active');
+        let totalSeats = schemeSvg.querySelectorAll('.active').length;
+        totalCost = totalSeats * cost;
+        spanCounter.textContent = totalCost;
+    }
+    
+});
